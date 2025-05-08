8fe53b4b921eabfc31696f660bf71657d486be9f
BauRoberts <bautistaroberts@gmail.com>
2025-05-08 14:43:13 -0300

    refactor: remove unused import of useRouter in Sidebar component

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useSession, signOut } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
